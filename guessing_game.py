62612a056e35cb70c0bd312629eb7d4d918ee203
Aniekan Ekanem <aekanem24@choate.edu>
2023-09-14 23:04:29 -0400

    guessing game homework

---
+++ guessing_game.py
@@ -0,0 +1,27 @@
+import random
+
+'''
+Name: Aniekan Ekanem
+Date: September 14, 2023
+Sources: Python While Loops: https://www.w3schools.com/python/python_while_loops.asp,
+Reflection: I enjoyed this assignment; it was fun
+Honor Code: On my honor I have neither given nor received unauthorized aid 
+'''
+
+#Intro
+print("Welcome to my guessing game! The number is between 1 and 200!")
+#Print random int between 1 and 200
+random_int = random.randint(1, 200)
+#User insert guess
+guess = int(input("Enter your guess \n>>> "))
+
+while random_int != guess:
+    if guess < random_int:
+        print("Too low, try again! ")
+        guess = int(input("Enter your guess again \n>>> "))
+    elif guess > random_int:
+        print("Too high, try again! ")
+        guess = int(input("Enter your guess again \n>>> "))
+    else:
+        break
+print("Congrats!!! you guesses the number!")
