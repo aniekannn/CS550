5f4aa551ad146415ab9876fa2a86d7414751f907
Aniekan Ekanem <aekanem24@choate.edu>
2023-09-18 12:05:15 -0400

    Created new folder for work/ files from week 1&2

---
+++ Week 1-2/loops.py
@@ -0,0 +1,15 @@
+'''
+to break infinte loops: ctrl c
+'''
+
+# Number 1
+for x in range (100, -110, -10):
+    print(x)
+
+# Number 2
+for x in range(7, 707, 7):
+    print(x)
+
+# Number 3
+for x in range(1, 99):
+    print(x*-1)
